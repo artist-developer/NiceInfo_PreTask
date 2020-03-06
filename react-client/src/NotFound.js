406381f7cd73dfd79931de273b884187c95aba0a
littlelittle_In <kimyi0108@ajou.ac.kr>
2020-03-06 10:18:18 +0900

    add Not Found Error MSG on Front-end codes.

---
+++ react-client/src/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//페이지를 잘못 들어갔을시 노출
+function NotFound() {
+  return (
+    <>
+      <h1>Not Found</h1>
+      <p>Sorry...</p>
+    </>
+  );
+}
+
+export default NotFound;
